ad4cb019d637085559ad868bbde6cf0d29bf73a0
Amy Russell <russell.amyh@gmail.com>
2018-04-10 17:29:40 -0600

    clean up

---
+++ src/helper/firebaseFunctions.test.js
@@ -0,0 +1,8 @@
+import * from './firebaseFunctions';
+
+describe('writeCurrency', () => {
+  it('should send data to firebase', () => {
+
+  })
+  it('should throw an error when the status is not ok')
+})
